7d63a43e4265ee494298b708482858171f179308
wei_lin <wei1234c@gmail.com>
2019-12-05 16:17:47 +0800

    add Track_Mouse_Movements.ipynb

---
+++ codes/test/test.py
@@ -0,0 +1,6 @@
+from usb_hid.orm.hid111 import HIDdevice
+
+
+dev = HIDdevice(1118, 1915)  # MS mouse
+rpt_descriptor = dev.report_descriptor_dbos[0].byte_array
+print(dev.report_descriptor_dbos[0].parse())
